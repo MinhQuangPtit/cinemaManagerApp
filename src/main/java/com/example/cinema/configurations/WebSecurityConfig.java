bcf7810c5a6771b537d7b28b392b0696950e8ed9
minhquangptit <vude100@gmail.com>
2023-12-08 13:23:02 +0700

    update spring security

--- src/main/java/com/example/cinema/configurations/WebSecurityConfig.java
+++ src/main/java/com/example/cinema/configurations/WebSecurityConfig.java
@@ -12,8 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
@@ -30,13 +29,30 @@ public class WebSecurityConfig {
                                 "user/login"
                             ).permitAll()
                             .requestMatchers(GET,"film/**").permitAll()
-                            .requestMatchers(GET,"service/**").permitAll()
-
                             .requestMatchers(POST,"film").hasAnyRole(Role.ADMIN)
+
+                            .requestMatchers(GET,"category/**").permitAll()
                             .requestMatchers(POST,"category").hasAnyRole(Role.ADMIN)
+                            .requestMatchers(PUT,"category/**").hasAnyRole(Role.ADMIN)
+                            .requestMatchers(DELETE,"category/**").hasAnyRole(Role.ADMIN)
+
+                            .requestMatchers(GET,"service/**").permitAll()
                             .requestMatchers(POST,"service").hasAnyRole(Role.ADMIN)
+                            .requestMatchers(PUT,"service/**").hasAnyRole(Role.ADMIN)
+                            .requestMatchers(DELETE,"service/**").hasAnyRole(Role.ADMIN)
+
+
+                            .requestMatchers(GET,"room").hasAnyRole(Role.ADMIN)
                             .requestMatchers(POST,"room").hasAnyRole(Role.ADMIN)
+                            .requestMatchers(PUT,"room/**").hasAnyRole(Role.ADMIN)
+                            .requestMatchers(DELETE,"room/**").hasAnyRole(Role.ADMIN)
+
+
+                            .requestMatchers(GET,"ticket**").hasAnyRole(Role.ADMIN,Role.USER)
                             .requestMatchers(POST,"ticket").hasAnyRole(Role.ADMIN,Role.USER)
+                            .requestMatchers(PUT,"ticket").hasAnyRole(Role.ADMIN,Role.USER)
+                            .requestMatchers(DELETE,"ticket").hasAnyRole(Role.ADMIN,Role.USER)
+
                             .anyRequest().authenticated();
                 });
         return httpSecurity.build();
